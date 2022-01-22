cc56511fc85a98bcecafbdc1a0944f68653ae15b
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-01-22 02:19:25 +0200

    feat: add header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import "./index.scss";
 
 const Header = () => {
+  const [conn, setConn] = useState("Connection String");
+  const [isSubmitted, setIsSubmitted] = useState(false);
   return (
     <header className="header">
       <nav className="header__nav">
         <ul className="header__links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">Tables</a></li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Tables</a>
+          </li>
         </ul>
       </nav>
-      <div className="header__db"></div>
+      <div className="header__db">
+        <input
+          value={conn}
+          disabled={isSubmitted}
+          onChange={(e) => setConn(e.target.value)}
+        ></input>
+        <button
+          disabled={isSubmitted}
+          onClick={() => (isSubmitted ? null : setIsSubmitted(true))}
+        >
+          Connect
+        </button>
+        <button onClick={() => (isSubmitted ? setIsSubmitted(false) : null)}>
+          Disconnect
+        </button>
+      </div>
     </header>
   );
 };
