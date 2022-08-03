2a11859076804405464de9d9c175f8333c8c8928
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-08-03 10:11:48 +0300

    feat: change environment variable names

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-const apiKey = process.env.REACT_APP_DB_API_KEY;
-const authDomain = process.env.REACT_APP_DB_AUTH_DOMAIN;
-const projectId = process.env.REACT_APP_DB_PROJECT_ID;
-const storageBucket = process.env.REACT_APP_DB_STORAGE_BUCKET;
-const messagingSenderId = process.env.REACT_APP_DB_MEASUREMENT_ID;
-const appId = process.env.REACT_APP_DB_APP_ID;
-const measurementId = process.env.REACT_APP_DB_MEASUREMENT_ID;
+const apiKey = process.env.REACT_APP_FIREBASE_DB_API_KEY;
+const authDomain = process.env.REACT_APP_FIREBASE_DB_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_FIREBASE_DB_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_FIREBASE_DB_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_FIREBASE_DB_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_FIREBASE_DB_APP_ID;
+const measurementId = process.env.REACT_APP_FIREBASE_DB_MEASUERMENT_ID;
 
 const firebaseConfig = {
   apiKey,
